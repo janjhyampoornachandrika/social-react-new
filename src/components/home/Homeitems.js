8c67fabd10daf151a08b503ceb238e5cba39e24c
npx <chandrikajanjhyam@gmail.com>
2024-04-15 10:59:56 +0530

    commit

---
+++ src/components/home/Homeitems.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Comment from '../comment/Comment'
+export default function Homeitems(props) {
+    const item = props.item
+    console.log(item.id)
+  return (
+    <div>
+        <div>{item.userId}</div>
+        <div>{item.title}</div>
+        <div>{item.body}</div>
+        
+        <Comment id={item.id}/>
+    </div>
+  )
+}
